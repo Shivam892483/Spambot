1e57909750f2d9c7c84170c4b5dbf55fbe37b2c5
˹Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ˼❤️ <111850575+Shivam892483@users.noreply.github.com>
2023-02-19 14:18:23 +0530

    Update start.py

--- DEADLYSPAM/plugins/start.py
+++ DEADLYSPAM/plugins/start.py
@@ -5,7 +5,7 @@ from telethon import events, Button
 from telethon.tl.custom import button
 from DEADLYSPAM import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, ALIVE_PIC, OWNER_ID, OWNER_NAME
 
-DEADLY_IMG = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/86f7674656cd38321316d.jpg"
+DEADLY_IMG = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/ad4762b9906450e728074.jpg"
 
 
 Deadly_Button = [
